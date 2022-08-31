1a888f09b1ef689e17a9de94e30998676071c78e
Edwin Cardona Villalba <ed.civil@gmail.com>
2022-08-30 19:03:34 -0500

    Primer acercamiento a Python

---
+++ datatype.py
@@ -0,0 +1,26 @@
+print ("Hello World")#Strings
+
+print(10) #Integer
+
+print(30.1) # Doble
+
+# Boolean
+print(type(True))
+False
+
+#List
+[10,20,30,55]
+["Hello","Bye","Hi"]
+[10,"Hello",True]
+[] # Lista vacia
+
+#Tuples
+print(type((10,20,30,40)))# inmutable, no se puede cambiar
+() #Tuples vacia
+
+#Dictorionies
+print(type({"Name": "Ryan",
+"Apellido": "Ray",
+"Apodo": "Fazt"}))
+
+print(type(None))
---
+++ helloworld.py
@@ -0,0 +1,1 @@
+print("Hello World")# Imprime Hello World
