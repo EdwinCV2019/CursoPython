e98520e03194e1aecb5d70add67d327671c49d8c
Edwin Cardona Villalba <ed.civil@gmail.com>
2022-08-31 19:59:12 -0500

    Variables y Strings

---
+++ Variables.py
@@ -0,0 +1,28 @@
+# Una variable es un "apodo" que se le da a un dato
+
+from msilib import PID_LASTPRINTED
+
+
+name = "Edwin"
+
+print (name)
+
+x= 100
+book = "Del amor y otros demonios"
+
+# otra forma de definir variables
+
+y, anime = 200, "Demon Slayer"
+
+print(y,anime)
+
+# Convenciones
+
+book_name = "Los cuatro acuerdos" # Snake Case
+bookName = "Como hacer amigos e influir sobre las personas" # Camel Case
+BookName = "Los doce pilares" # Pascal Case
+
+PI=3.1416  # Se escriben las constantes en mayusculas
+
+# las variables pueden ser reasignadas
+
---
+++ strings.py
@@ -0,0 +1,7 @@
+myStr="Hello World"
+
+#print (dir(myStr))
+
+print(myStr.upper()) #convertir todo a mayuscula
+
+print(myStr.lower()) #todo en minusculas
